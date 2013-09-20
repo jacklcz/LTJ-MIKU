c0ebafce096431f7ebfc621df8fea68e3700d07d
liangzhu <chengzhu.liang@langtaojin.com>
2013-09-20 21:29:42 +0800

    add getbeforeOrAfterdate func

--- common/dateutils.py
+++ common/dateutils.py
@@ -17,6 +17,7 @@ def get_currentdate():
     return datetime.datetime.now()
 
 
+
 """
 get_format_date has two args ,default :date  = current_date and  format_str = '%Y-%m-%d'
 """
@@ -24,6 +25,16 @@ def get_format_date(date=get_currentdate().timetuple(),format_str='%Y-%m-%d'):
     return time.strftime(format_str,date)
 
 
+
+"""
+getbeforeOrAfter date , if you need get before one day, you can set plug = -1'
+"""
+def get_beforeOrAfter(plus,date=get_format_date()):
+    tm = time.strptime(date,'%Y-%m-%d')
+    ts = time.localtime(time.mktime(tm)+plus*86400)
+    return time.strftime('%Y-%m-%d',ts);
+
+
 if __name__ == "__main__":
-    #print(get_format_date())
-    pass
+    print(get_beforeOrAfter(-1,'1990-10-23'))
+    #pass
